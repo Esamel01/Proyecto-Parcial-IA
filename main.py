0af225d22d56bf3de5f7b479bf61dceec0a34d2f
Esamel01 <esamelmercedes9@gmail.com>
2025-07-07 21:48:15 -0400

    esta es la inalizacion

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ import os
 import heapq
 from pygame.locals import *
 
+pygame.init()
+pygame.mixer.init()
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Roguelike - Directional Shooting")
+clock = pygame.time.Clock()
+FPS = 60
